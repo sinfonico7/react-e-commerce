3cf913a542adce3df1ac6c9fa858312f03f9dea0
Felipe Inostroza <felipeinostroza@NI-NTB-083.local>
2021-03-25 22:11:20 -0300

    Styling items menu

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,7 +7,7 @@ import './menu-item.styles.scss';
         backgroundImage: `url(${imageUrl})`
     }} className={ `${size} menu-item` }>
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>Shop Now!</span>
         </div>
     </div>
