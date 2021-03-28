b353008c965a455fa9275ea6d01bc9e125ecc98b
Felipe Inostroza <felipeinostroza@NI-NTB-083.local>
2021-03-28 02:15:07 -0300

    Adding Heaing Menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header   from './components/header/header.component';
 
 import './App.css';
 import { Route, Switch } from 'react-router';
@@ -10,6 +11,7 @@ import { Route, Switch } from 'react-router';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
