876ee1ac0155915b343ad8cd04b0b714b22ef559
Felipe Inostroza <felipeinostroza@NI-NTB-083.local>
2021-04-03 20:15:46 -0300

    implementing sign in method on fire base

--- src/components/sign-in/sign.component.jsx
+++ src/components/sign-in/sign.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './sign.styles.scss';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import { auth ,  signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -14,12 +14,19 @@ class SignIn extends React.Component{
 
     }
 
-    handleSubmit = () => {
-        //event.preventDefault();
-        this.setState({email:'',password:''});
+    handleSubmit = async event => {
+        event.preventDefault();
+        const {email , password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword( email , password );
+            this.setState({email:'',password:''});
+
+        }catch(error){
+            console.log(error);
+        }
     }
 
-    handleChange = event => {
+    handleChange = async event => {
         const {value , name} = event.target;
         this.setState({  [name] : value })
     }
