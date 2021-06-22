0d344b1e63c5a06ba6d1d26ac0a7fedef96779e0
Felipe Inostroza <finostroza@nisum.com>
2021-06-21 20:31:08 -0400

    backup for change of device

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import './header.styles.scss';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 const Header = ({currentUser}) => (
     <div className='header'>
@@ -23,8 +25,9 @@ const Header = ({currentUser}) => (
                  : 
                 <Link to='/signin'>SIGN IN</Link>
             }
-            
+            <CartIcon/>
         </div>
+        <CartDropdown/>
     </div>
 
 );
